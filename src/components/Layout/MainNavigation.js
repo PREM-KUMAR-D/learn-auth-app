5eea246971b54997e4adae64ba87731e223e6c42
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-12-05 23:37:56 +0530

    completed change password

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -18,24 +18,18 @@ const MainNavigation = () => {
       <nav>
         <ul>
           <li>
-            {isLoggedIn &&
+            
 
               <Link to='/auth'>Login</Link>
-            }
+            
           </li>
           <li>
-            {
-              isLoggedIn &&
 
               <Link to='/profile'>Profile</Link>
-            }
+
           </li>
           <li>
-            {
-              isLoggedIn &&
-
               <button>Logout</button>
-            }
           </li>
         </ul>
       </nav>
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,37 @@
+import { useContext, useRef } from 'react';
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const passwordRef =useRef(null);
+  const authCtx = useContext(AuthContext);
+
+  const submitFormHandler  = async (event)=>{
+    event.preventDefault();
+    const enteredPass = passwordRef.current.value;
+
+    const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_FIRE_BASE_API_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+            idToken: authCtx.token,
+            password:enteredPass,
+            returnSecureToken: false, 
+        }),
+        headers: {
+          'Content-Type':'application/json'
+        }
+      }
+    )
+
+
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitFormHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={passwordRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
